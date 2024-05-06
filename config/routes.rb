92761092049e623d3f1876d3dc9c6c3aa23ef769
austinj33 <164961440+austinj33@users.noreply.github.com>
2024-05-06 02:39:10 +0000

    video done

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get("/photos", { :controller => "photos", :action => "index"})
   get("/photos/:path_id", { :controller => "photos", :action => "show"})
   get("/delete_photo/:toast_id", { :controller => "photos", :action => "cya"})
+  post("/insert_photo", { :controller => "photos", :action => "create"})
+  post("/update_photo/:modify_id", { :controller => "photos", :action => "update"})
 end
